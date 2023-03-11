4d5bb67aa688d98a5403bf0a5b81f5d898631a0b
Ronie Arauco <ronie.paolo@krealo.pe>
2023-03-11 01:00:52 -0500

    Changes in directory tree

--- app/front/src/containers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './header/Header';
-export { default as Demo } from './demo/Demo';
-export { default as Footer } from './footer/Footer';
-export { default as Description } from './description/Description';
